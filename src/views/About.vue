f79f38912c5e7a43f0cc47d45d43e708797839a1
Baz Chalk <boruch11@gmail.com>
2021-01-25 09:50:04 +0200

    nest deeply

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      <p>Some more info</p>
+      <div>
+        <div>
+          <p>
+            Really nested
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
