191af33bda129516f4746915b949951170ecc8c5
Boruch Chalk <boruch11@gmail.com>
2021-06-02 10:55:54 +0300

    ignore node modules

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 module.exports = {
 	devServer: {
 		watchOptions: {
-			aggregateTimeout: 50,
-			poll: 50
+			aggregateTimeout: 300,
+			poll: 50,
+			ignored: /node_modules/
 		},
 		allowedHosts: [
 			".bagelstudio.co",
