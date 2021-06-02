6a357272313cf8b4efcabed138c0dc8f7e1d5645
Boruch Chalk <boruch11@gmail.com>
2021-06-02 10:16:28 +0300

    change up

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-	configureWebpack: {
-		devServer: {
-			watchOptions: {
-				aggregateTimeout: 100,
-				poll: 100
-			},
-			allowedHosts: [
-				".bagelstudio.co",
-				".bageldb.com"
-			]
-		}
+	devServer: {
+		watchOptions: {
+			aggregateTimeout: 50,
+			poll: 50
+		},
+		allowedHosts: [
+			".bagelstudio.co",
+			".bageldb.com"
+		]
 	}
 }
